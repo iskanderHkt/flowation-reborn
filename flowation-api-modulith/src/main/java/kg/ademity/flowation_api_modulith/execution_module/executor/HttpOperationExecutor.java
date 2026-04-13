e2b82ea8bdd98adf1cfa3d29853a397cfd7e9af0
ialymov <ialymov@nurtelecom.kg>
2026-04-13 12:11:21 +0600

    fix

--- flowation-api-modulith/src/main/java/kg/ademity/flowation_api_modulith/execution_module/executor/HttpOperationExecutor.java
+++ flowation-api-modulith/src/main/java/kg/ademity/flowation_api_modulith/execution_module/executor/HttpOperationExecutor.java
@@ -9,10 +9,15 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Component;
 
+import javax.net.ssl.SSLContext;
+import javax.net.ssl.TrustManager;
+import javax.net.ssl.X509TrustManager;
+import java.net.ProxySelector;
 import java.net.URI;
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
+import java.security.cert.X509Certificate;
 import java.time.Duration;
 import java.util.HashMap;
 import java.util.Map;
@@ -22,7 +27,27 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class HttpOperationExecutor implements OperationExecutor<HttpOperationConfig> {
 
-    private static final HttpClient httpClient = HttpClient.newHttpClient();
+    private static final HttpClient httpClient = HttpClient.newBuilder()
+            .proxy(ProxySelector.getDefault())
+            .sslContext(buildTrustAllSslContext())
+            .build();
+
+    private static SSLContext buildTrustAllSslContext() {
+        try {
+            TrustManager[] trustAll = new TrustManager[]{
+                new X509TrustManager() {
+                    public X509Certificate[] getAcceptedIssuers() { return new X509Certificate[0]; }
+                    public void checkClientTrusted(X509Certificate[] chain, String authType) {}
+                    public void checkServerTrusted(X509Certificate[] chain, String authType) {}
+                }
+            };
+            SSLContext ctx = SSLContext.getInstance("TLS");
+            ctx.init(null, trustAll, null);
+            return ctx;
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to build trust-all SSLContext", e);
+        }
+    }
     private final ObjectMapper objectMapper;
 
     @Override
